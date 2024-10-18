e40dc11a75e70544f23bddbc7feb809b9036af27
ssasukek <kevinlu3234@gmail.com>
2024-10-18 00:19:03 -0700

    ...

---
+++ lab4/task1.py
@@ -0,0 +1,41 @@
+from hashlib import sha256
+
+
+def sha256_hash(input_str):
+    return
+
+
+def truncate_hash(hash_str, bits):
+    return 
+
+
+def find_collision(bits, max_attempts):
+    seen = []
+    return
+
+
+def task_1a():
+    print("Task 1a: SHA256 shashes of arbitrary inputs")
+    return
+
+
+def task_1b():
+    print("Task 1b: Strings with Hamming distance of 1")
+    for i in range(1,3):
+
+        print s1, s2, h1, h2
+    return
+
+def task_1c():
+    print("Task 1c: Finding collisions for truncated hashes")
+    bits = []
+    time = []
+    inputs = []
+    for bits in range(8, 50):
+    
+    return
+
+if __name__ == "__main__":
+    task_1a()
+    task_1b()
+    task_1c()
